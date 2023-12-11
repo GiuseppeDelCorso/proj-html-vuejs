9fa5fd19bbc7673dcba1548ffa64383853974383
giuseppedelcorso <Giuseppedecoboolean@gmail.com>
2023-12-11 17:48:04 +0100

    aggiunto inizio main

--- src/components/AppMain1.vue
+++ src/components/AppMain1.vue
@@ -9,6 +9,33 @@ export default {
 
 </script>
 
-<template></template>
+<template>
+    <div class="container">
+        <div class="row mt-4">
+            <div class="col-6 bgRed">
+                <h1 class="mt-6">Udemy Affilate Sales</h1>
+                <p>Monetize your audience and attract new customers with Udemy!</p>
+            </div>
+            <div class="col-6 px-0">
+                <img class="imgJuboStyle" src="../assets/slide-1.jpg" alt="">
+            </div>
+        </div>
+    </div>
+</template>
 
-<style scoped></style>
+<style scoped>
+.mt-6 {
+    margin-top: 110px;
+}
+
+.bgRed {
+    background-color: #ff4450;
+    color: white;
+}
+
+.imgJuboStyle {
+    width: 100%;
+    height: 350px;
+
+}
+</style>
